f9ed98f6389cde9afcf0c559bcf9c516015a6bf7
liedsonlm7 <liedsonlm7@gmail.com>
2026-08-01 15:34:06 -0300

    feat: chess layer and printing the board

--- src/main/java/boardgame/Board.java
+++ src/main/java/boardgame/Board.java
@@ -27,4 +27,12 @@ public class Board {
     public void setColumns(int columns) {
         this.columns = columns;
     }
+
+    public Piece piece(int row, int colum) {
+        return pieces[row][colum];
+    }
+
+    public Piece piece(Position position) {
+        return pieces[position.getRow()][position.getColumn()];
+    }
 }
